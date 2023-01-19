2801ce8f8c8e60b34000d180fb771c23cd03f6ce
Mamang Touthang <huebart16@gmail.com>
2023-01-19 15:30:53 +0530

    domain update

--- server/config/index.js
+++ server/config/index.js
@@ -9,7 +9,7 @@ const config = {
   cookieOption: {
     expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
     // httpOnly: true,
-    domain: ".up.railway.app",
+    domain: ".railway.app",
   },
 };
 
