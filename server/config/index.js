13b8cecca1d6aa319f1e935475286dc2e994caf5
Mamang Touthang <huebart16@gmail.com>
2023-01-19 16:28:12 +0530

    domain name change

--- server/config/index.js
+++ server/config/index.js
@@ -9,7 +9,7 @@ const config = {
   cookieOption: {
     expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
     // httpOnly: true,
-    domain: "railway.app",
+    domain: "notes-keeping-app-ui-production.up.railway.app",
     secure: true,
   },
 };
